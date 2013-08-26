d3b5f01f3b7e150d4db3068a006dbd9e9bc53e4d
Otavio Medeiros <otaviorm@gmail.com>
2013-08-26 14:46:02 -0300

    Initializer generator without zoho api key configured #5

--- lib/generators/crm_integration_generator.rb
+++ lib/generators/crm_integration_generator.rb
@@ -4,7 +4,7 @@ class CrmIntegrationGenerator < Rails::Generators::Base
       require 'ruby_zoho'
 
       RubyZoho.configure do |config|
-        config.api_key = 'b45751924308154cabece0310eface13'
+        config.api_key = 'your api key here'
       end
     FILE
   end
